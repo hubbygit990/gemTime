7bf292dc7cd48f4e0a555861fba9f953bd606c6b
root <root@localhost>
2023-06-11 10:59:10 +0100

    auto publish 1686477550243

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Add all your JS here
+
+
+
+
+
+
+for ( i = 10; i >= 0; i--) {
+amILocal = "ur about to fink smth stupid";
+console.log(amILocal);
+};
+// none of these ;s matter i think lol
+//the one thats not there after i-- does tho
